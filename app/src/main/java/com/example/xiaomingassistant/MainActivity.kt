b1616bd9f8679db836ddd6039c1f5a30c9ec45e2
FengGJSNW <creeper20070807@163.com>
2026-04-07 21:23:42 +0800

    macos端：提交主界面底栏ui,登陆界面ui

--- app/src/main/java/com/example/xiaomingassistant/MainActivity.kt
+++ app/src/main/java/com/example/xiaomingassistant/MainActivity.kt
@@ -17,31 +17,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            XiaomingAssistantTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    XiaomingAssistantTheme {
-        Greeting("Android")
+        setContentView(R.layout.main_interface_footer)
     }
 }
