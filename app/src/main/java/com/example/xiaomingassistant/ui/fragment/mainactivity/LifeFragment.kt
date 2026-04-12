50f264bb8c4a472565f5ff241b9e91924e313a97
FengGJSNW <creeper20070807@163.com>
2026-04-12 21:15:19 +0800

    标记：第一个防出问题的存档点

--- app/src/main/java/com/example/xiaomingassistant/ui/fragment/mainactivity/LifeFragment.kt
+++ app/src/main/java/com/example/xiaomingassistant/ui/fragment/mainactivity/LifeFragment.kt
@@ -2,12 +2,20 @@ package com.example.xiaomingassistant.ui.fragment.mainactivity
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
-import androidx.core.widget.NestedScrollView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import com.example.xiaomingassistant.R
-import com.google.android.material.card.MaterialCardView
+
+class LifeFragment : Fragment(R.layout.main_interface_life) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
 
-class LifeFragment : Fragment() {
+
+    }
 }
